28820d9e84dc73e49c6108730c07bb867ec9d77a
sypher256 <136737245+sypher256@users.noreply.github.com>
2023-10-25 11:04:28 -0700

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,53 @@
+/*let listTodo = [{
+    name: 'washe dihes',
+    duedate: '2022-22-22'
+}];
+*/
+let listTodo = JSON.parse(localStorage.getItem('listTodo'));
+console.log(listTodo);
+
+
+TodoLoop();
+function TodoLoop(){
+    let todoListHtml = '';
+    //localStorage.setItem('listTodo', JSON.stringify(listTodo))
+
+    for(let i = 0; i<listTodo.length; i++){
+    const TodoObject = listTodo[i];
+    //const {name} = TodoObject;
+    const {duedate, name} = TodoObject;
+    let html = `
+    <p class="todo-p">
+    ${name}   ${duedate}
+    <button class="delete-button" onclick="
+    listTodo.splice(${i},1);
+    localStorage.setItem('listTodo', JSON.stringify(listTodo))
+    TodoLoop();
+    ">Delete</button>
+    </p>
+    `;
+    todoListHtml += html;
+    
+    
+}
+document.querySelector('.todoElements').innerHTML = `${todoListHtml}`;
+console.log(todoListHtml);
+}
+
+function add(){
+    let dateTimeInput = document.querySelector('.date-time-input').value;
+    console.log(dateTimeInput);
+    let inputValue = document.querySelector('.inputArea').value;
+     let inputValueElement = document.querySelector('.inputArea');
+    let newTodoObject = {
+        name: inputValue,
+        duedate: dateTimeInput
+    }
+    
+    listTodo.push(newTodoObject);
+    console.log(listTodo);
+    localStorage.setItem('listTodo', JSON.stringify(listTodo))
+    TodoLoop();
+    inputValueElement.value = '';
+    
+}
